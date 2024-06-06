a3f06a5eccff498636ceb75df10ceecc45f19e50
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2024-06-06 15:54:38 +0200

    Add a issrc style compile.bat.

---
+++ compile.bat
@@ -0,0 +1,56 @@
+@echo off
+
+rem  Inno Setup
+rem  Copyright (C) 1997-2024 Jordan Russell
+rem  Portions by Martijn Laan
+rem  For conditions of distribution and use, see LICENSE.TXT.
+rem
+rem  Batch file to compile isscint.dll
+
+setlocal
+
+if exist compilesettings.bat goto compilesettingsfound
+:compilesettingserror
+echo compilesettings.bat is missing or incomplete. It needs to be created
+echo with the following line, adjusted for your system:
+echo.
+echo   set VSTOOLSROOT=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools
+goto failed2
+
+:compilesettingsfound
+set VSTOOLSROOT=
+call .\compilesettings.bat
+if "%VSTOOLSROOT%"=="" goto compilesettingserror
+
+rem -------------------------------------------------------------------------
+
+cd win32
+if errorlevel 1 goto exit
+
+echo VsDevCmd.bat
+call "%VSTOOLSROOT%\VsDevCmd.bat"
+if errorlevel 1 goto exit
+
+echo - Scintilla.dll
+nmake -f scintilla.mak clean
+if errorlevel 1 goto failed
+nmake -f scintilla.mak
+if errorlevel 1 goto failed
+
+echo - Renaming Scintilla.dll
+cd ..\bin
+if errorlevel 1 goto failed
+move Scintilla.dll isscint.dll
+if errorlevel 1 goto failed
+
+echo Success!
+cd ..
+goto exit
+
+:failed
+echo *** FAILED ***
+cd ..
+:failed2
+exit /b 1
+
+:exit
