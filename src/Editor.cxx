aee6df88fb4856e9bc4cbf9f4d35a9b2808af664
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-08-19 07:00:24 +0200

    Apply fix from https://sourceforge.net/p/scintilla/bugs/2487/

--- src/Editor.cxx
+++ src/Editor.cxx
@@ -2435,6 +2435,7 @@ void Editor::RestoreSelection(Sci::Position newPos, UndoRedo history) {
 				}
 			}
 			newPos = -1; // Used selection from stack so don't use position returned from undo/redo.
+			Redraw();
 		}
 	}
 	if (newPos >= 0)
